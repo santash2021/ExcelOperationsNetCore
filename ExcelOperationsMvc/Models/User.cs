585514e095ba84d0f485f841914a5458edcad1f4
e.djumadylov <e.djumadylov@it.salyk.kg>
2022-05-21 16:07:35 +0600

    Project root

---
+++ ExcelOperationsMvc/Models/User.cs
@@ -0,0 +1,9 @@
+namespace ExcelOperationsMvc.Models;
+
+public class User
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public int Age { get; set; }
+    public string? Address { get; set; }
+}
